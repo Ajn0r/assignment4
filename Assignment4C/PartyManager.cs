c74c39887785c3761026edc5cdf3a24c879b9642
Ronja <ronjasjogren@hotmail.com>
2023-11-12 19:28:48 +0100

    add all (empty) methods to PartyManager

--- Assignment4C/PartyManager.cs
+++ Assignment4C/PartyManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -72,6 +73,78 @@ namespace PartyOrganizer
         // --------------------------------------------------------------
         //                           Methods
         // --------------------------------------------------------------
+        // Method to add a new guest to the guest list
+        public bool AddNewGuest(string firstName, string lastName)
+        {
+            return false;
+        }
+
+        // Method to calculate the total cost of the party
+        public double CalculateTotalCost()
+        {
+            return 0.0;
+        }
+
+        // Method to calculate the total fee of the party
+        public double CalculateTotalFee()
+        {
+               return 0.0;
+        }
+
+        // Method to change the name of a guest at a given index
+        public bool ChangeAt(int index, string firstName, string lastName)
+        {
+            return false;
+        }
+
+        // Method to check if a given index is valid
+        public bool CheckIndex(int index)
+        {
+            return false;
+        }
+
+        // Method to delete a guest at a given index
+        public bool DeleteAt(int index)
+        {
+            return false;
+        }
+
+        // Method to find a vacant position in the array
+        private int FindVacantPosition()
+        {
+            return 0;
+        }
+
+        // Method to return the full name of a guest formated with last name first
+        // in upper case, a comma and then the first name.
+        private string FullName(string firstName, string lastName)
+        {
+            return string.Format(lastName.ToUpper() + ", " + firstName);
+        }
+
+        // Method to get the guest list
+        public string[] GetGuestList()
+        {
+            return null;
+        }
+
+        // Method to get an item(guest) at a given index
+        public string GetItemAt(int index)
+        {
+            return null;
+        }
+
+        // Method to move all elements one step to the left from a given index
+        private void MoveElementsOneStepLeft(int index)
+        {
+
+        }
+
+        // Method that returns the number of guests
+        private int NumberOfGuests()
+        {
+            return guestList.Length;
+        }
 
     }
 }
